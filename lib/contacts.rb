f8ef528418d8c176c38069d49c52f8ffb3aeb04d
Adam Beatty <adam.t.beatt@gmail.com>
2020-08-19 18:10:12 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -4,7 +4,7 @@ require 'pry'
   # contacts = {
   #   "Jon Snow" => {
   #     name: "Jon",
-  #     email: "jon_snow@thewall.we", 
+  #     email: "jon_snow@thewall.we",
   #     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
   #   },
   #   "Freddy Mercury" => {
@@ -13,7 +13,13 @@ require 'pry'
   #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
   #   }
   # }
-  
+
 def remove_strawberry(contacts)
+  contacts.each do | contact_name, data |
+    contact_name[:favorite_ice_cream_flavors].each_with_index do | flavor, i |
+      if flavor == "strawberry"
+        contact_name[:favorite_ice_cream_flavors].delete_at(i)
+    end
+  end
 
 end
